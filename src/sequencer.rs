eaab68a832247c9b6c1f87f503218613b7a37d15
Etienne Tremel <995474+etiennetremel@users.noreply.github.com>
2023-02-04 22:13:39 +0100

    feat: use match for keys instead if

--- src/sequencer.rs
+++ src/sequencer.rs
@@ -1,4 +1,3 @@
-use crate::app;
 use embedded_hal::adc::OneShot;
 use mcp49xx::Command;
 use rtic::mutex_prelude::*;
@@ -6,6 +5,7 @@ use rtt_target::rprintln;
 use smart_leds::RGB;
 use systick_monotonic::*;
 
+use crate::app;
 use crate::constants::*;
 use crate::keyboard::*;
 use crate::led::*;
@@ -17,7 +17,8 @@ pub enum RecordingMode {
     Note,
 }
 
-pub(crate) fn keyboard_ctrl(mut cx: app::keyboard_ctrl::Context) {
+// keyboard key detection controller
+pub(crate) fn keyboard_ctrl(cx: app::keyboard_ctrl::Context) {
     cx.local.keyboard.read();
 
     (
@@ -27,142 +28,160 @@ pub(crate) fn keyboard_ctrl(mut cx: app::keyboard_ctrl::Context) {
         cx.shared.recording_mode,
     )
         .lock(|tracks, current_track, recording_cursor, recording_mode| {
-            // switch recording mode
-            if cx.local.keyboard.key_event.function == Some(FunctionKey::FN1)
-                && cx.local.keyboard.key_event.modifier == Some(ModifierKey::SHIFT)
-            {
-                if *recording_mode == RecordingMode::Note {
-                    rprintln!("Pressed Fn1+Shift, switched to STEP RECORDING");
-                    *recording_mode = RecordingMode::Step
-                } else {
-                    rprintln!("Pressed Fn1+Shift, switched to NOTE RECORDING");
-                    tracks[*current_track].cursor = 0;
-                    *recording_mode = RecordingMode::Note
+            match (
+                cx.local.keyboard.key_event.function,
+                cx.local.keyboard.key_event.modifier,
+                cx.local.keyboard.key_event.nav,
+                cx.local.keyboard.key_event.code,
+            ) {
+                // switch recording mode
+                (Some(FunctionKey::FN1), Some(ModifierKey::SHIFT), None, None) => {
+                    if *recording_mode == RecordingMode::Note {
+                        rprintln!("Pressed Fn1+Shift, switched to STEP RECORDING");
+                        *recording_mode = RecordingMode::Step
+                    } else {
+                        rprintln!("Pressed Fn1+Shift, switched to NOTE RECORDING");
+                        tracks[*current_track].cursor = 0;
+                        *recording_mode = RecordingMode::Note
+                    }
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_KEY_PRESS_DELAY_MS,
+                    ))
+                    .unwrap();
                 }
-                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                    KEYBOARD_KEY_PRESS_DELAY_MS,
-                ));
-                return;
-            }
 
-            // switch track mode (CV vs Gate)
-            if cx.local.keyboard.key_event.function == Some(FunctionKey::FN1)
-                && cx.local.keyboard.key_event.nav == Some(NavKey::FORWARD)
-            {
-                rprintln!("Pressed Fn1+Forward, toggle current track mode");
-                tracks[*current_track].toggle_mode();
-                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                    KEYBOARD_KEY_PRESS_DELAY_MS,
-                ));
-                return;
-            }
+                // switch track mode (CV vs Gate)
+                (Some(FunctionKey::FN1), None, Some(NavKey::FORWARD), None) => {
+                    rprintln!("Pressed Fn1+Forward, toggle current track mode");
+                    tracks[*current_track].toggle_mode();
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_KEY_PRESS_DELAY_MS,
+                    ))
+                    .unwrap();
+                }
 
-            // select next track
-            if cx.local.keyboard.key_event.modifier == Some(ModifierKey::SHIFT)
-                && cx.local.keyboard.key_event.nav == Some(NavKey::FORWARD)
-            {
-                rprintln!("Pressed Shift+Forward, select next track");
-                *current_track = if *current_track < TRACKS_COUNT - 1 {
-                    *current_track + 1
-                } else {
-                    0
-                };
+                // select next track
+                (None, Some(ModifierKey::SHIFT), Some(NavKey::FORWARD), None) => {
+                    rprintln!("Pressed Shift+Forward, select next track");
+                    *current_track = if *current_track < TRACKS_COUNT - 1 {
+                        *current_track + 1
+                    } else {
+                        0
+                    };
 
-                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                    KEYBOARD_KEY_PRESS_DELAY_MS,
-                ));
-                return;
-            }
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_KEY_PRESS_DELAY_MS,
+                    ))
+                    .unwrap();
+                }
 
-            // clear pattern for current track
-            if cx.local.keyboard.key_event.function == Some(FunctionKey::FN2)
-                && cx.local.keyboard.key_event.modifier == Some(ModifierKey::SHIFT)
-            {
-                rprintln!("Pressed Fn2+Shift, clear pattern");
-                tracks[*current_track].clear();
-                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                    KEYBOARD_KEY_PRESS_DELAY_MS,
-                ));
-                return;
-            }
+                // clear pattern for current track
+                (Some(FunctionKey::FN2), Some(ModifierKey::SHIFT), None, None) => {
+                    rprintln!("Pressed Fn2+Shift, clear pattern");
+                    tracks[*current_track].clear();
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_KEY_PRESS_DELAY_MS,
+                    ))
+                    .unwrap();
+                }
 
-            // select previous track
-            if cx.local.keyboard.key_event.modifier == Some(ModifierKey::SHIFT)
-                && cx.local.keyboard.key_event.nav == Some(NavKey::BACK)
-            {
-                rprintln!("Pressed Shift+Back, select previous track");
-                *current_track = if *current_track > 0 {
-                    *current_track - 1
-                } else {
-                    TRACKS_COUNT - 1
-                };
+                // select previous track
+                (None, Some(ModifierKey::SHIFT), Some(NavKey::BACK), None) => {
+                    rprintln!("Pressed Shift+Back, select previous track");
+                    *current_track = if *current_track > 0 {
+                        *current_track - 1
+                    } else {
+                        TRACKS_COUNT - 1
+                    };
 
-                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                    KEYBOARD_KEY_PRESS_DELAY_MS,
-                ));
-                return;
-            }
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_KEY_PRESS_DELAY_MS,
+                    ))
+                    .unwrap();
+                }
 
-            if let Some(code) = cx.local.keyboard.key_event.code {
-                if *recording_mode == RecordingMode::Note {
-                    if let Some(note) = cx.local.keyboard.match_note(code) {
-                        rprintln!("Pressed note {:?}", note);
-                        tracks[*current_track]
-                            .set_note(*recording_cursor, note)
-                            .set_gate(*recording_cursor, Gate::ON);
-                        if *recording_cursor == tracks[*current_track].pattern.len() {
-                            *recording_cursor = 0;
+                // switch track
+                (Some(FunctionKey::FN1), None, None, Some(CodeKey)) => {
+                    if let Some(code) = cx.local.keyboard.key_event.code {
+                        if let Some(step) = cx.local.keyboard.match_step(code) {
+                            if cx.local.keyboard.key_event.function == Some(FunctionKey::FN1)
+                                && step < TRACKS_COUNT
+                            {
+                                rprintln!("Pressed Fn1+{:?}, switch track", step);
+                                *current_track = step;
+                                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                                    KEYBOARD_KEY_PRESS_DELAY_MS,
+                                ))
+                                .unwrap();
+                            }
+                        }
+                    }
+                }
+
+                // randomize gates with probability based on the key pressed
+                (Some(FunctionKey::FN2), None, None, Some(CodeKey)) => {
+                    if let Some(code) = cx.local.keyboard.key_event.code {
+                        if let Some(step) = cx.local.keyboard.match_step(code) {
+                            if step <= 8 {
+                                rprintln!("Pressed Fn2+{}, randomize pattern", step);
+                                tracks[*current_track].randomize(step as f64 / 8.0);
+                                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                                    KEYBOARD_KEY_PRESS_DELAY_MS,
+                                ))
+                                .unwrap();
+                            }
+                        }
+                    }
+                }
+
+                (None, None, None, Some(CodeKey)) => {
+                    if let Some(code) = cx.local.keyboard.key_event.code {
+                        if *recording_mode == RecordingMode::Note {
+                            // write notes
+                            if let Some(note) = cx.local.keyboard.match_note(code) {
+                                rprintln!("Pressed note {:?}", note);
+                                tracks[*current_track]
+                                    .set_note(*recording_cursor, note)
+                                    .set_gate(*recording_cursor, Gate::ON);
+                                if *recording_cursor == tracks[*current_track].pattern.len() {
+                                    *recording_cursor = 0;
+                                } else {
+                                    *recording_cursor += 1;
+                                }
+                                app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                                    KEYBOARD_KEY_PRESS_DELAY_MS,
+                                ))
+                                .unwrap();
+                            }
                         } else {
-                            *recording_cursor += 1;
-                        }
-                        app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                            KEYBOARD_KEY_PRESS_DELAY_MS,
-                        ));
-                        return;
-                    }
-                } else {
-                    if let Some(step) = cx.local.keyboard.match_step(code) {
-                        // switch track
-                        if cx.local.keyboard.key_event.function == Some(FunctionKey::FN1)
-                            && step < TRACKS_COUNT
-                        {
-                            rprintln!("Pressed Fn1+{:?}, switch track", step);
-                            *current_track = step;
-                            app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                                KEYBOARD_KEY_PRESS_DELAY_MS,
-                            ));
-                            return;
-                        }
-
-                        // randomize gates with probability based on the key pressed
-                        if cx.local.keyboard.key_event.function == Some(FunctionKey::FN2)
-                            && step <= 8
-                        {
-                            rprintln!("Pressed Fn2+{}, randomize pattern", step);
-                            tracks[*current_track].randomize(step as f64 / 8.0);
-                            app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                                KEYBOARD_KEY_PRESS_DELAY_MS,
-                            ));
-                            return;
-                        }
-
-                        // toggle step
-                        if step < tracks[*current_track].pattern.len() {
-                            rprintln!("Pressed Step {}", step);
-                            tracks[*current_track].toggle_step(step);
-                            app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
-                                KEYBOARD_KEY_PRESS_DELAY_MS,
-                            ));
-                            return;
+                            // toggle step
+                            if let Some(step) = cx.local.keyboard.match_step(code) {
+                                if step < tracks[*current_track].pattern.len() {
+                                    rprintln!("Pressed Step {}", step);
+                                    tracks[*current_track].toggle_step(step);
+                                    app::keyboard_ctrl::spawn_after(
+                                        systick_monotonic::ExtU64::millis(
+                                            KEYBOARD_KEY_PRESS_DELAY_MS,
+                                        ),
+                                    )
+                                    .unwrap();
+                                }
+                            }
                         }
                     }
+                }
+
+                (_, _, _, _) => {
+                    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(
+                        KEYBOARD_REFRESH_MS,
+                    ))
+                    .unwrap();
                 }
             }
         });
-    app::keyboard_ctrl::spawn_after(systick_monotonic::ExtU64::millis(KEYBOARD_REFRESH_MS));
 }
 
-pub(crate) fn led_ctrl(mut cx: app::led_ctrl::Context) {
+pub(crate) fn led_ctrl(cx: app::led_ctrl::Context) {
     (
         cx.shared.led_driver,
         cx.shared.tracks,
@@ -172,7 +191,7 @@ pub(crate) fn led_ctrl(mut cx: app::led_ctrl::Context) {
     )
         .lock(
             |led_driver, tracks, current_track, recording_cursor, recording_mode| {
-                let mut track = tracks[*current_track];
+                let track = tracks[*current_track];
                 if *recording_mode == RecordingMode::Note {
                     note_recording(led_driver, track, *current_track, *recording_cursor);
                 } else {
@@ -181,43 +200,28 @@ pub(crate) fn led_ctrl(mut cx: app::led_ctrl::Context) {
             },
         );
 
-    app::led_ctrl::spawn_after(systick_monotonic::ExtU64::millis(LED_REFRESH_MS));
+    app::led_ctrl::spawn_after(systick_monotonic::ExtU64::millis(LED_REFRESH_MS)).unwrap();
 }
 
-pub(crate) fn tick(mut cx: app::tick::Context, instant: fugit::TimerInstantU64<100>) {
+// tick move play cursor ahead by 1 step on each track
+pub(crate) fn tick(cx: app::tick::Context, instant: fugit::TimerInstantU64<100>) {
     (cx.shared.tracks, cx.shared.current_track).lock(|tracks, current_track| {
         // move 1 step forward on each tracks
         for i in 0..TRACKS_COUNT {
             tracks[i].tick();
         }
         rprintln!("CURRENT TRACK [{:?}]", *current_track,);
-
-        // rprintln!(
-        //     "CURRENT TRACK [{:?}] | {:?} | {:?} | {:?} | {:?} | {:?} | {:?} | {:?} | {:?}",
-        //     *current_track,
-        //     tracks[0].pattern[tracks[0].cursor],
-        //     tracks[1].pattern[tracks[1].cursor],
-        //     tracks[2].pattern[tracks[2].cursor],
-        //     tracks[3].pattern[tracks[3].cursor],
-        //     tracks[4].pattern[tracks[4].cursor],
-        //     tracks[5].pattern[tracks[5].cursor],
-        //     tracks[6].pattern[tracks[6].cursor],
-        //     tracks[7].pattern[tracks[7].cursor],
-        // );
     });
 
     app::cv_ctrl::spawn().unwrap();
     app::gate_reset::spawn_at(instant + *cx.local.gate_length).unwrap();
 
+    // call next tick
     let next_instant = instant + *cx.local.step_length;
-    // rprintln!(
-    //     "half {:?}, next: {:?}",
-    //     instant + *cx.local.trigger_length,
-    //     next_instant
-    // );
     app::tick::spawn_at(next_instant, next_instant).unwrap();
 }
 
+// define led lighting when in note recording mode
 fn note_recording(
     led_driver: &mut LedDriver,
     track: Track,
@@ -238,16 +242,19 @@ fn note_recording(
         .write();
 }
 
+// define led lighting when in step recording mode
 fn step_recording(led_driver: &mut LedDriver, track: Track, current_track: usize) {
     led_driver.clear();
 
     // button state
     for step in 0..track.pattern.len() {
+        // display current active gate
         if track.pattern[step].gate == Gate::ON {
             led_driver.set_gate_on(step);
-            if track.mode == TrackMode::CV {
-                led_driver.set_note(step, track.pattern[step].note);
-            }
+        }
+        // display note being played
+        if track.mode == TrackMode::CV && track.cursor == step {
+            led_driver.set_note(track.pattern[step].note);
         }
     }
 
@@ -258,7 +265,8 @@ fn step_recording(led_driver: &mut LedDriver, track: Track, current_track: usize
         .write();
 }
 
-pub(crate) fn cv_ctrl(mut cx: app::cv_ctrl::Context) {
+// write Gate/CV value for each DAC
+pub(crate) fn cv_ctrl(cx: app::cv_ctrl::Context) {
     let cmd = Command::default();
 
     (
@@ -301,28 +309,11 @@ pub(crate) fn cv_ctrl(mut cx: app::cv_ctrl::Context) {
                 dac2.send(spi_dac, cmd.value(match_note_to_cv(step_track1.note)))
                     .unwrap();
             }
-
-            // rprintln!(
-            //     "CURRENT TRACK [{:?}] | {:?} | {:?} | {:?} | {:?} | {:?} | {:?} | {:?} | {:?}",
-            //     *current_track,
-            //     tracks[0].pattern[tracks[0].cursor],
-            //     tracks[1].pattern[tracks[1].cursor],
         });
-
-    // rprintln!("Set position {:?} and {:?}", val, val2);
-    //
-    // cx.local
-    //     .dac1
-    //     .send(cx.local.spi_dac, cmd.value(val))
-    //     .unwrap();
-    // cx.local
-    //     .dac2
-    //     .send(cx.local.spi_dac, cmd.value(val2))
-    //     .unwrap();
-    // app::cv_ctrl::spawn_after(systick_monotonic::ExtU64::millis(500)).unwrap();
 }
 
-pub(crate) fn gate_reset(mut cx: app::gate_reset::Context) {
+// reset all gate after trigger
+pub(crate) fn gate_reset(cx: app::gate_reset::Context) {
     let cmd = Command::default();
 
     (
@@ -344,7 +335,8 @@ pub(crate) fn gate_reset(mut cx: app::gate_reset::Context) {
 
 // return led position based on a given note
 fn match_note_to_cv(note: Note) -> u16 {
-    match (note) {
+    // TODO: adjust with correct CV value
+    match note {
         Note::C => 0,
         Note::D => 400,
         Note::E => 800,
